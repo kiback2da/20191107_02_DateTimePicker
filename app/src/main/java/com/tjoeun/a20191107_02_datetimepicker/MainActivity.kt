e124a0180d2c5be2a6df03966b2df637c0a37027
kiback2da <kiback2da@steconsulting.co.kr>
2019-11-07 22:13:42 +0900

    Calendar / SimpleDateFormat 을 활용   - month 값을 직접 사용하지 않는 방향으로 코드 수정

--- app/src/main/java/com/tjoeun/a20191107_02_datetimepicker/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191107_02_datetimepicker/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : BaseActivity() {
@@ -19,7 +20,18 @@ class MainActivity : BaseActivity() {
         dateBtn.setOnClickListener {
             var datePicketDialog = DatePickerDialog(this,
                 DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
-                    dateTxt.text = "${year}년 ${month+1}월 ${dayOfMonth}일"
+                    var selectedDate = Calendar.getInstance()
+
+                    //원하는 항목별로 직접 입력
+                    //selectedDate.set(Calendar.YEAR, year)
+
+                    //년월일을 한번에 집어넣기
+                    selectedDate.set(year,month,dayOfMonth)
+
+                    var sdf = SimpleDateFormat("yyyy년 M월 d일")
+                    var resultStr = sdf.format(selectedDate.time)
+
+                    dateTxt.text = resultStr
                 }, 2019,Calendar.NOVEMBER,7)
             datePicketDialog.show()
         }
